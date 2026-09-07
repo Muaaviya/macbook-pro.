a6636d61ce9bc1079b8ee6a3d988b55b6ea396bf
Muaaviya <muaaviyaansari007@gmail.com>
2026-09-07 23:38:52 +0530

    format

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,51 @@
-import { Canvas } from '@react-three/fiber'
-import React from 'react'
-import { OrbitControls, Environment, ScrollControls } from '@react-three/drei'
-import MacContainer from './Components/MacContainer'
-import './style.css'
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import { OrbitControls, Environment, ScrollControls } from "@react-three/drei";
+import MacContainer from "./Components/MacContainer";
+import "./style.css";
 
 const App = () => {
   return (
     <div className="w-full h-screen font-['Helvetica_Now_Display']">
       <div className="navbar line flex gap-10 pt-10 pb-3 absolute top-0 left-1/2 -translate-x-1/2">
-        {['iPhone', 'iPad', 'prodcuts', 'mac', 'ios', 'iPhone', 'iPad', 'prodcuts', 'mac', 'ios'].map((e) => (<a className="font-[400] text-sm capitalize">{e}</a>))}
+        {[
+          "iPhone",
+          "iPad",
+          "prodcuts",
+          "mac",
+          "ios",
+          "iPhone",
+          "iPad",
+          "prodcuts",
+          "mac",
+          "ios",
+        ].map((e) => (
+          <a className="font-[400] text-sm capitalize">{e}</a>
+        ))}
       </div>
       <div className="absolute flex flex-col items-center text-white top-40 left-1/2 -translate-x-1/2">
-        <h3 className="masked text-7xl tracking-tighter font-[700]">macbook pro.</h3>
+        <h3 className="masked text-7xl tracking-tighter font-[700]">
+          macbook pro.
+        </h3>
         <h5>Oh so pro!</h5>
-        <p className="text-center w-3/4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt repellendus dolorum voluptates totam!</p>
+        <p className="text-center w-3/4">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          repellendus dolorum voluptates totam!
+        </p>
       </div>
       <Canvas camera={{ fov: 12, position: [0, -10, 220] }}>
         {/* <OrbitControls /> */}
-        <Environment files={["https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr",]} />
+        <Environment
+          files={[
+            "https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr",
+          ]}
+        />
         <ScrollControls pages={3}>
           <MacContainer />
         </ScrollControls>
       </Canvas>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Components/MacContainer.jsx
+++ src/Components/MacContainer.jsx
@@ -1,31 +1,33 @@
-import { useGLTF, useScroll, useTexture } from '@react-three/drei'
-import { useFrame } from '@react-three/fiber';
-import React from 'react'
-import * as THREE from 'three'
+import { useGLTF, useScroll, useTexture } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import React from "react";
+import * as THREE from "three";
 
 const MacContainer = () => {
-    let model = useGLTF('./mac.glb');
-    let tex = useTexture('./red.jpg')
-    let meshes = {};
+  let model = useGLTF("./mac.glb");
+  let tex = useTexture("./red.jpg");
+  let meshes = {};
 
-    model.scene.traverse((e) => {
-        meshes[e.name] = e;
-    });
+  model.scene.traverse((e) => {
+    meshes[e.name] = e;
+  });
 
-    meshes.screen.rotation.x = THREE.MathUtils.degToRad(180);
-    meshes.matte.material.map = tex;
-    meshes.matte.material.emissiveIntensity = 0;
-    meshes.matte.material.metalness = 0;
-    meshes.matte.material.roughness = 1;
-    let data = useScroll()
+  meshes.screen.rotation.x = THREE.MathUtils.degToRad(180);
+  meshes.matte.material.map = tex;
+  meshes.matte.material.emissiveIntensity = 0;
+  meshes.matte.material.metalness = 0;
+  meshes.matte.material.roughness = 1;
+  let data = useScroll();
 
-    useFrame((state, delta) => {
-        meshes.screen.rotation.x = THREE.MathUtils.degToRad(180 - data.offset * 90)
-    });
+  useFrame((state, delta) => {
+    meshes.screen.rotation.x = THREE.MathUtils.degToRad(180 - data.offset * 90);
+  });
 
-    return <group position={[0, -10, 20]}>
-        <primitive object={model.scene} />
+  return (
+    <group position={[0, -10, 20]}>
+      <primitive object={model.scene} />
     </group>
-}
+  );
+};
 
-export default MacContainer
+export default MacContainer;
